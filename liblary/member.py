9d70c2d46c1816331f72f718e10be163479f0941
MUHAMMAD OBIDOV <magaobidov@gmail.com>
2024-06-12 14:40:45 +0300

    Add Member class

---
+++ liblary/member.py
@@ -0,0 +1,21 @@
+class Member:
+    def __init__(self, name, member_id):
+        self.name = name
+        self.member_id = member_id
+        self.borrowed_books = []
+
+    def borrow_book(self, book):
+        if book.borrow():
+            self.borrowed_books.append(book)
+            return True
+        return False
+
+    def return_book(self, book):
+        if book in self.borrowed_books:
+            book.return_book()
+            self.borrowed_books.remove(book)
+            return True
+        return False
+
+    def __str__(self):
+        return f"Member: {self.name}, ID: {self.member_id}"
